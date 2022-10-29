29df4db762a5683f2596a2178acf0c1d374e20f4
Brett Earle <brettearle@gmail.com>
2022-10-29 21:28:14 +1030

    counting beans exercise completed with tests.

---
+++ countingBeans/countBeans.js
@@ -0,0 +1,37 @@
+const countB = (str) => {
+  if (str === undefined) {
+    throw new Error("no string provided");
+  } else if (typeof str !== "string") {
+    throw new Error("arg not a string");
+  } else {
+    let count = 0;
+    for (let index = 0; index < str.length; index++) {
+      const letter = str[index];
+      if (letter === "B") {
+        count++;
+      }
+    }
+    return count;
+  }
+};
+
+const countChar = (str, char) => {
+  if (str === undefined || char === undefined) {
+    throw new Error("arguments dont match parameters");
+  } else if (typeof str !== "string" || typeof char !== "string") {
+    throw new Error("arg are not strings");
+  } else {
+    let count = 0;
+    for (let index = 0; index < str.length; index++) {
+      const letter = str[index];
+      count = count + check(letter, char);
+    }
+    return count;
+  }
+};
+
+const check = function (strLetter, providedChar) {
+  return strLetter === providedChar ? 1 : 0;
+};
+
+export { countB, countChar };
---
+++ countingBeans/countBeans.test.js
@@ -0,0 +1,57 @@
+import { countB, countChar } from "./countBeans";
+
+test("countB should be type function", () => {
+  expect(typeof countB).toEqual("function");
+});
+
+test("countB called with no args throws 'no string provided'", () => {
+  expect(() => {
+    countB();
+  }).toThrow("no string provided");
+});
+
+test("countB called with arg that isn't type string throws 'arg not a string'", () => {
+  expect(() => {
+    countB(1);
+  }).toThrow("arg not a string");
+});
+
+test("countB called with 'ABC' should return 1", () => {
+  const got = countB("ABC");
+  const expected = 1;
+  expect(got).toEqual(expected);
+});
+
+test("countB called with 'BBB' should return 3", () => {
+  const got = countB("BBB");
+  const expected = 3;
+  expect(got).toEqual(expected);
+});
+
+test("countChar should be type function", () => {
+  expect(typeof countChar).toEqual("function");
+});
+
+test("countChar called with no args throws 'no string provided'", () => {
+  expect(() => {
+    countChar();
+  }).toThrow("arguments dont match parameters");
+});
+
+test("countChar called with arg that isn't type string throws 'arg are not strings'", () => {
+  expect(() => {
+    countChar(1, "g");
+  }).toThrow("arg are not strings");
+});
+
+test("countChar called with 'abcd' and 'd' returns 1", () => {
+  const got = countChar("abcd", "d");
+  const expected = 1;
+  expect(got).toEqual(expected);
+});
+
+test("countChar called with 'ddddd' and 'd' returns 5", () => {
+  const got = countChar("ddddd", "d");
+  const expected = 5;
+  expect(got).toEqual(expected);
+});
