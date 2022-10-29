fac00a84fdd0c9b8e27d529e656bbdae261f4a55
Brett Earle <brettearle@gmail.com>
2022-10-29 15:43:47 +1030

    hash pyramid complete

--- hashPyramid/hashPyramid.js
+++ hashPyramid/hashPyramid.js
@@ -1,12 +1,28 @@
+const getHashString = (index) => {
+  const lineCount = index;
+  let hashString = "";
+  for (let index = 0; index < lineCount; index++) {
+    hashString = hashString + "#";
+  }
+  return hashString;
+};
+
 const buildPyramid = (numOfLines) => {
   let returnStr = "";
-  for (let index = 0; index < numOfLines; index++) {
-    returnStr = returnStr + "#";
+  for (let index = 1; index <= numOfLines; index++) {
+    const hashString = getHashString(index);
+    const spacer = index === 0 || index === numOfLines ? "" : "\n";
+    returnStr = returnStr + hashString + spacer;
   }
   return returnStr;
 };
 
 export default function HashPyramid(lines) {
-  let str = buildPyramid(lines);
-  return str;
+  if (Number.isInteger(lines) === false) {
+    throw new Error("must be called with a int");
+  } else {
+    let str = buildPyramid(lines);
+    console.log(str);
+    return str;
+  }
 }
--- hashPyramid/hashPyramid.test.js
+++ hashPyramid/hashPyramid.test.js
@@ -1,4 +1,3 @@
-import { string } from "yargs";
 import HashPyramid from "./hashPyramid";
 
 test("expect hash pyramid to be a function", () => {
@@ -6,7 +5,7 @@ test("expect hash pyramid to be a function", () => {
 });
 
 test("hashPyramid returns a string", () => {
-  const got = HashPyramid();
+  const got = HashPyramid(0);
   expect(typeof got).toEqual("string");
 });
 
@@ -14,4 +13,23 @@ test("hash pyramid with lines arg 1 returns '#'", () => {
   const got = HashPyramid(1);
   const expected = "#";
   expect(got).toStrictEqual(expected);
+});
+
+test("hash pyramid produces 3 lines, first with #, 2nd with ##, 3rd with ###", () => {
+  const got = HashPyramid(3);
+  const expected = "#\n##\n###";
+  expect(got).toEqual(expected);
+});
+
+test("hash pyramid produces a 10 line left aligned pyramid growing per line by 1", () => {
+  const got = HashPyramid(10);
+  const expected =
+    "#\n##\n###\n####\n#####\n######\n#######\n########\n#########\n##########";
+  expect(got).toEqual(expected);
+});
+
+test("hash pyramid to throw error if arg numOfLines isnt a int", () => {
+  expect(() => {
+    HashPyramid("");
+  }).toThrow(new Error("must be called with a int"));
 });
