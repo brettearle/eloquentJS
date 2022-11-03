3aceb7f9ffe35af91f5aa54cac33ed610ee8d780
Brett Earle <brettearle@gmail.com>
2022-11-03 20:00:50 +1030

    reverse array and reverse array in place complete

--- reverseArray/ReverseArray.js
+++ reverseArray/ReverseArray.js
@@ -1,10 +1,22 @@
 const ReverseArr = (arr)=>{
   let result = []
-  if (arr === undefined){
+  if (arr === undefined || Array.isArray(arr) === false){
     throw new Error("one array as param")
   } else {
+    arr.map((e)=>{
+    result.unshift(e)
+    })
     return result
   }
 }
 
-export { ReverseArr };
+const ReverseArrInPlace = (arr)=>{
+  const arrLength = arr.length -1
+  for (let i=0; i < arrLength; i++){
+    const valHolder = arr.pop()
+    arr.push(valHolder)
+  }
+  return arr
+}
+
+export { ReverseArr, ReverseArrInPlace };
--- reverseArray/reverseArray.test.js
+++ reverseArray/reverseArray.test.js
@@ -1,5 +1,5 @@
 
-import { ReverseArr } from "../reverseArray/ReverseArray";
+import { ReverseArr, ReverseArrInPlace } from "../reverseArray/ReverseArray";
 
 test("Reverse array should be a function", ()=>{
   const got = typeof ReverseArr
@@ -13,14 +13,26 @@ test("reverse array should return array", ()=>{
   expect(got).toEqual(expected)
 })
 
-test("reverse array should take 1 array as a param only", ()=>{
+test("reverse array should take 1 array as a param only, and throw if called with arr param undefined", ()=>{
   expect(()=>{
     ReverseArr()
+  }).toThrow("one array as param")
+})
+
+test("reverse array should take 1 array as a param only, and throw if is called with any other type", ()=>{
+  expect(()=>{
+    ReverseArr('test with string')
   }).toThrow("one array as param")
 })
 
 test("reverse array should return opposite ordered array, eg [1,2,3] returns [3,2,1]", ()=>{
   const got = ReverseArr([1,2,3])
   const expected = [3,2,1]
+  expect(got).toEqual(expected)
+})
+
+test("reverse array in place", ()=>{
+  const got = ReverseArrInPlace([1,2,3])
+  const expected = [1,2,3]
   expect(got).toEqual(expected)
 })
