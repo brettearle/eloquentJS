30240f192e24ffab822114cab019f8bc82c8d5b2
Brett Earle <brettearle@gmail.com>
2022-11-06 14:33:26 +1030

    deep comparison done with test suite

--- deepComparison/DeepComparison.js
+++ deepComparison/DeepComparison.js
@@ -1,12 +1,57 @@
+function checkTypeArray(a,b){
+  if (Array.isArray(a) && Array.isArray(b)){
+    return true
+  } else {
+    return false
+  }
+}
+
+function checkArrayDeep(a,b){
+  if (a.length !== b.length){
+    return false
+  } else {
+    let result = true
+    const length = a.length
+    for (let index = 0; index < length; index++) {
+      const aEl = a[index]
+      const bEl = b[index]
+      if (aEl !== bEl){
+        result = false
+      } 
+    }
+    return result
+  }
+}
+
+function checkObjectDeep(a,b){
+  const aKeys = Object.keys(a)
+  const bKeys = Object.keys(b)
+  const aVals = Object.values(a)
+  const bVals = Object.values(b)
+  if (checkArrayDeep(aKeys, bKeys) === false){
+    return false
+  } else {
+    return checkArrayDeep(aVals, bVals)
+  }
+}
+
 const DeepComparison = (a, b) => {
   if (a === undefined || b === undefined) {
     throw new Error("Error: args list");
     
   } else {
-    if (a !== null && b !== null){
-    return a === b 
-    } else if (a === null && b !== null || a !== null && b === null){
+    if (a === null && b !== null || a !== null && b === null){
       return false
+    } else if (a === null && b === null) {
+      return true
+    } else if (typeof a === 'object' && typeof b === 'object') {
+      if (checkTypeArray(a,b)){
+       return checkArrayDeep(a,b)
+      } else {
+       return checkObjectDeep(a,b)
+      }  
+    } else if (a !== null && b !== null){
+    return a === b 
     }
   }
 } 
--- deepComparison/deepComparison.test.js
+++ deepComparison/deepComparison.test.js
@@ -36,4 +36,46 @@ describe("DeepComparison", () => {
     const exp = false
     expect(got).toEqual(exp)
   })
+
+  test("when comparing 2 null values returns true", () => {
+    const got = DeepComparison(null, null)
+    const exp = true
+    expect(got).toEqual(exp)
+  })
+
+  test("called with 2 arrays of different lengths returns false", () => {
+    const got = DeepComparison([1,2,3], [1,2])
+    const exp = false
+    expect(got).toEqual(exp)
+  })
+
+  test("called with 2 arrays equal length & different values returns false", () => {
+    const got = DeepComparison([1,2,3], [1,2,5])
+    const exp = false
+    expect(got).toEqual(exp)
+  })
+
+  test("called with 2 arrays equal length & same values returns true", () => {
+    const got = DeepComparison([1,2,3], [1,2,3])
+    const exp = true
+    expect(got).toEqual(exp)
+  })
+
+  test("called with 2 objects with different keys returns false", () => {
+    const got = DeepComparison({val: 1, strng: "hey"}, {val: 1, str: "hey"})
+    const exp = false
+    expect(got).toEqual(exp)
+  })
+
+  test("called with 2 objects with same keys and different values returns false", () => {
+    const got = DeepComparison({val: 1, str: "hey"}, {val: 2, str: "hey"})
+    const exp = false
+    expect(got).toEqual(exp)
+  })
+
+  test("called with 2 objects with same keys and same values returns true", () => {
+    const got = DeepComparison({val: 1, str: "hey"}, {val: 1, str: "hey"})
+    const exp = true
+    expect(got).toEqual(exp)
+  })
 })
