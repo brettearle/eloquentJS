d98613e6eaa87ff662ad2c4bef795512b36e70f0
Brett Earle <brettearle@gmail.com>
2022-11-07 22:43:52 +1030

    loop works with tests

--- customLoop/loop.js
+++ customLoop/loop.js
@@ -1,5 +1,15 @@
-function Loop() {
-  return ""
+function Loop(value, testFunc, updateFunc, bodyFunc) {
+  if (value === undefined || testFunc === undefined || updateFunc === undefined || bodyFunc === undefined) {
+    throw new Error("Error: args. (value, testFunc, upDateFunc, bodyFunc)")
+  } else {
+    let count = value
+    let result = value
+    while (testFunc(count) === true){
+     let r = bodyFunc(result)
+      count = updateFunc(count)
+      result = r
+    }
+    return result
 }
-
+}
 export {Loop}
--- customLoop/loop.test.js
+++ customLoop/loop.test.js
@@ -7,4 +7,16 @@ describe("loop", ()=>{
     expect(typeof Loop).toEqual('function')
   })
 
+  test("takes in a value, test func, update func, body func", ()=>{
+    expect(()=>{
+      Loop()
+    }).toThrow("Error: args. (value, testFunc, upDateFunc, bodyFunc)")
+  })
+
+  test("loop(3, n => n > 0, n => n - 1,n => n + 1)", ()=>{
+    const got = Loop(3, n => n > 0, n => n - 1,n => n + 1)
+    const want = 6
+    expect(got).toEqual(want)
+  })
+
 })
